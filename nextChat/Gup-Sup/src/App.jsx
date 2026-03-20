6244acb4fc96adf5bbc88d0f5c0c2777a1e68893
krashanyadav <krashanyadav5@gmail.com>
2026-03-21 00:42:07 +0530

    new

--- nextChat/Gup-Sup/src/App.jsx
+++ nextChat/Gup-Sup/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
-import Profile from "./pages/profile"
+import Profile from "./pages/Profil"
 
 function App(){
 
